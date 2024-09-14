a732955ef027d26e23fabc7a65b8db9ae33f7d6c
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-14 23:57:36 +0200

    if applied, will ADD: another unstyled component

--- expense-splitter/src/pages/Groups.jsx
+++ expense-splitter/src/pages/Groups.jsx
@@ -1,14 +1,17 @@
+import ExpenseBar from "../components/GroupExpenseBar";
 import GroupMembers from "../components/GroupMembers";
 import GroupName from "../components/GroupName";
 import GroupSmallExpenseCard from "../components/GroupSmallExpenseCard";
 
 function Groups() {
-  return <div>
-    <GroupName/>
-    <GroupSmallExpenseCard/>
-    <GroupMembers />
-
-  </div>;
+  return (
+    <div>
+      <GroupName />
+      <GroupSmallExpenseCard />
+      <GroupMembers />
+      <ExpenseBar expense={500} budget={1000}  />
+    </div>
+  );
 }
 
 export default Groups;
