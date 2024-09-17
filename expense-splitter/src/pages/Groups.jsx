d24a368b78888ff0a164f1c50d37491e162d121a
Mandla-tech <moyo7mandla@gmail.com>
2024-09-17 22:15:44 +0200

    resolve merge conflict in Groups component caused by incoming blank lines below div.

--- expense-splitter/src/pages/Groups.jsx
+++ expense-splitter/src/pages/Groups.jsx
@@ -5,15 +5,13 @@ import GroupName from "../components/GroupName";
 import GroupSmallExpenseCard from "../components/GroupSmallExpenseCard";
 
 function Groups() {
-<<<<<<< HEAD
   return <div className="p-6 space-y-6 bg-blizzard-blue min-h-screen">
 
     <GroupName/>
     <GroupSmallExpenseCard/>
     <GroupMembers />
-=======
->>>>>>> 15b5ad3acbb2a52597a5ae09622101d9f09791ed
-
+  </div>
 }
 
 export default Groups;
+
