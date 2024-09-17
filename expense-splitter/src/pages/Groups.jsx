9607c8f143bb0e1afcf1ee2843dcb8ec9e4dd97a
Mandla-tech <moyo7mandla@gmail.com>
2024-09-17 21:53:33 +0200

    feat: add new unstyled components

--- expense-splitter/src/pages/Groups.jsx
+++ expense-splitter/src/pages/Groups.jsx
@@ -4,7 +4,7 @@ import GroupSmallExpenseCard from "../components/GroupSmallExpenseCard";
 
 function Groups() {
   return <div className="p-6 space-y-6 bg-blizzard-blue min-h-screen">
-    
+
     <GroupName/>
     <GroupSmallExpenseCard/>
     <GroupMembers />
