70701bf2d9f669500b9356046f42b4a13fcca812
Mandla-tech <moyo7mandla@gmail.com>
2024-09-17 22:42:40 +0200

    resolved merge conflict in Groups

--- expense-splitter/src/pages/Groups.jsx
+++ expense-splitter/src/pages/Groups.jsx
@@ -3,13 +3,8 @@ import GroupName from "../components/GroupName";
 import GroupSmallExpenseCard from "../components/GroupSmallExpenseCard";
 
 function Groups() {
-  return <div className="p-6 space-y-6 bg-blizzard-blue min-h-screen">
 
-    <GroupName/>
-    <GroupSmallExpenseCard/>
-    <GroupMembers />
-
-  </div>;
+  
 }
 
 export default Groups;
