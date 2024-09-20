100928f072a7700dbb640b9a202ba074e3cb1cf0
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-20 23:13:02 +0200

    if applied, will ADD: new and styled small FriendName.jsx compoment

--- expense-splitter/src/components/Friends/FriendsMutualGroups.jsx
+++ expense-splitter/src/components/Friends/FriendsMutualGroups.jsx
@@ -2,7 +2,7 @@ import FriendsEachMutualGroup from "./FriendsEachMutualGroup";
 
 function FriendsMutualGroups() {
   return (
-    <article className="w-[30rem] bg-red-50 m-6 rounded-xl">
+    <article className="w-[30rem] bg-red-50 rounded-xl">
       <div className="py-3 px-4 flex justify-between">
         <p className="font-semibold">4 Mutual groups</p>
         <button className="text-primary bg-blizzard-blue px-2 py-1 rounded-md">View all</button>
--- expense-splitter/src/pages/Friends.jsx
+++ expense-splitter/src/pages/Friends.jsx
@@ -1,9 +1,11 @@
 import FriendsTable from "../components/Friends/FriendsTable"
 import FriendsMutualGroups from "../components/Friends/FriendsMutualGroups"
+import FriendName from "../components/Friends/FriendName"
 
 function Friends() {
     return (
-        <section>
+        <section className="flex flex-col gap-5 m-6">
+            <FriendName />
             <FriendsMutualGroups />
             <FriendsTable />
 
