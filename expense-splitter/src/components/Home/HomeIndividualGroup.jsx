0dc929f2527057c4332809c2adb1eb656ba82a92
predrag jandric <predrag.jandric.bg@gmail.com>
2024-09-27 00:45:58 +0200

    will ADD: modal on Homepage that opens the form to create a new group. modal itself opens by clicking the button on the homepage next to existing groups

--- expense-splitter/src/components/Home/HomeIndividualGroup.jsx
+++ expense-splitter/src/components/Home/HomeIndividualGroup.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 function HomeIndividualGroup({ group }) {
   return (
     <div className="p-4 border border-black w-72 h-36">
-      <h2 className="pb-4">{group.name}</h2>
+      <h2 className="">{group.name}</h2>
+      <p className="pb-2">members: {group.members}</p>
       <Link
         className="border border-emerald-600 hover:bg-emerald-200 p-2 text-xl"
         to={`/groups/${group.id}`}
